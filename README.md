ab3b97124f601b0fc29654e6383da5eee83b5343
Jay Long <jaylong@Jays-MacBook-Air.local>
2021-07-20 17:14:27 -0500

    oops.

--- README.md
+++
@@ -1,2 +0,0 @@
-# revenant-hollow-admin-app
-A native app used by haunt admins to configure IOT nodes on the LAN and track mobile user movements throughout the location.
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Revenant Hollow Admin',
+  appName: 'admin-app',
   webDir: 'build',
   bundledWebRuntime: false
 };
