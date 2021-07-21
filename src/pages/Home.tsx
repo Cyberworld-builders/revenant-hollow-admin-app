cbaac5cdcc83135a15378a69fa1d0931b87a06d6
Jay Long <jaylong@Jays-MacBook-Air.local>
2021-07-20 22:01:40 -0500

    geolocation buttion, android build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,39 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonGrid,
+    IonCol,
+    IonRow,
+} from "@ionic/react";
+import React from "react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+import GeolocationButton from "../components/GeolocationButton";
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Geolocation</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Geolocation</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+
+                <div className="container">
+                    <GeolocationButton />
+                </div>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;
